a975da75ac0995f80ef2b29f62e42545a2d68050
lex roodsar <a.vahid@campus.lmu.de>
2020-01-15 17:14:07 +0100

    spaltenzahl hinzugefügt

--- frontend/src/data/kaiser.js
+++ frontend/src/data/kaiser.js
@@ -1,136 +1,157 @@
 export const pos_ferdinand_ii = 8;
 
-export const kaiser = [
-  {
-      "ID": "A3",
-      "NAME": "Anna",
-      "start": "1616 Jul 21",
-      "end": "1676 Sep 11"
-  },
-  {
-      "ID": "C5",
-      "NAME": "Karl\u00a0V.",
-      "start": "1500 Feb 24",
-      "end": "1558 Sep 21"
-  },
-  {
-      "ID": "C6",
-      "NAME": "Karl\u00a0VI.",
-      "start": "1685 Okt 01",
-      "end": "1740 Okt 20"
-  },
-  {
-      "ID": "CF",
-      "NAME": "Klaudia Felizitas",
-      "start": "1653 Mai 30",
-      "end": "1676 Apr 08"
-  },
-  {
-      "ID": "E2",
-      "NAME": "Eleonora\u00a0II.",
-      "start": "1598 Sep 23",
-      "end": "1655 Jun 27"
-  },
-  {
-      "ID": "E3",
-      "NAME": "Eleonora\u00a0III.",
-      "start": "1630 Nov 18",
-      "end": "1686 Dez 06"
-  },
-  {
-      "ID": "ES",
-      "NAME": "Ernst",
-      "start": "1553 Jun 15",
-      "end": "1595 Feb 20"
-  },
-  {
-      "ID": "F1",
-      "NAME": "Ferdinand\u00a0I.",
-      "start": "1503 Mar 10",
-      "end": "1564 Jul 25"
-  },
-  {
-      "ID": "F2",
-      "NAME": "Ferdinand\u00a0II.",
-      "start": "1578 Jul 09",
-      "end": "1637 Feb 15"
-  },
-  {
-      "ID": "F3",
-      "NAME": "Ferdinand\u00a0III.",
-      "start": "1608 Jul 13",
-      "end": "1657 Apr 02"
-  },
-  {
-      "ID": "F4",
-      "NAME": "Ferdinand\u00a0IV.",
-      "start": "1633 Sep 08",
-      "end": "1654 Jul 09"
-  },
-  {
-      "ID": "FR",
-      "NAME": "Friedrich\u00a0III.",
-      "start": "1415 Sep 21",
-      "end": "1493 Aug 19"
-  },
-  {
-      "ID": "J1",
-      "NAME": "Joseph\u00a0I.",
-      "start": "1678 Jul 26",
-      "end": "1711 Apr 17"
-  },
-  {
-      "ID": "JC",
-      "NAME": "Johann Karl",
-      "start": "1605 Nov 01",
-      "end": "1619 Dez 26"
-  },
-  {
-      "ID": "L1",
-      "NAME": "Leopold\u00a0I.",
-      "start": "1640 Jun 09",
-      "end": "1705 Mai 05"
-  },
-  {
-      "ID": "LW",
-      "NAME": "Leopold Wilhelm",
-      "start": "1614 Jan 05",
-      "end": "1662 Nov 20"
-  },
-  {
-      "ID": "M1",
-      "NAME": "Maximilian\u00a0I.",
-      "start": "1459 Mar 22",
-      "end": "1519 Jan 12"
-  },
-  {
-      "ID": "M2",
-      "NAME": "Maximilian\u00a0II.",
-      "start": "1527 Jul 31",
-      "end": "1576 Okt 12"
-  },
-  {
-      "ID": "MAT",
-      "NAME": "Antonia",
-      "start": "1669 Jan 18",
-      "end": "1692 Dez 24"
-  },
-  {
-      "ID": "MS",
-      "NAME": "Matthias",
-      "start": "1557 Feb 24",
-      "end": "1619 Mar 20"
-  },
-  {
-      "ID": "MT",
-      "NAME": "Maria Theresia",
-      "start": "1717 Mai 13",
-      "end": "1780 Nov 29"
-  },
-  {
-      "ID": "R2",
-      "NAME": "Rudolf\u00a0II.",
-      "start": "1552 Jul 18",
-      "end": "1612 Jan 20"
-  }
-  ]
+export const kaiser = [{
+        "ID": "A3",
+        "NAME": "Anna",
+        "start": "1616 Jul 21",
+        "end": "1676 Sep 11",
+        "spalte": "0"
+    },
+    {
+        "ID": "C5",
+        "NAME": "Karl\u00a0V.",
+        "start": "1500 Feb 24",
+        "end": "1558 Sep 21",
+        "spalte": "0"
+    },
+    {
+        "ID": "C6",
+        "NAME": "Karl\u00a0VI.",
+        "start": "1685 Okt 01",
+        "end": "1740 Okt 20",
+        "spalte": "0"
+    },
+    {
+        "ID": "CF",
+        "NAME": "Klaudia Felizitas",
+        "start": "1653 Mai 30",
+        "end": "1676 Apr 08",
+        "spalte": "2"
+    },
+    {
+        "ID": "E2",
+        "NAME": "Eleonora\u00a0II.",
+        "start": "1598 Sep 23",
+        "end": "1655 Jun 27",
+        "spalte": "0"
+    },
+    {
+        "ID": "E3",
+        "NAME": "Eleonora\u00a0III.",
+        "start": "1630 Nov 18",
+        "end": "1686 Dez 06",
+        "spalte": "0"
+    },
+    {
+        "ID": "ES",
+        "NAME": "Ernst",
+        "start": "1553 Jun 15",
+        "end": "1595 Feb 20",
+        "spalte": "1"
+    },
+    {
+        "ID": "F1",
+        "NAME": "Ferdinand\u00a0I.",
+        "start": "1503 Mar 10",
+        "end": "1564 Jul 25",
+        "spalte": "0"
+    },
+    {
+        "ID": "F2",
+        "NAME": "Ferdinand\u00a0II.",
+        "start": "1578 Jul 09",
+        "end": "1637 Feb 15",
+        "spalte": "0"
+    },
+    {
+        "ID": "F3",
+        "NAME": "Ferdinand\u00a0III.",
+        "start": "1608 Jul 13",
+        "end": "1657 Apr 02",
+        "spalte": "0"
+    },
+    {
+        "ID": "F4",
+        "NAME": "Ferdinand\u00a0IV.",
+        "start": "1633 Sep 08",
+        "end": "1654 Jul 09",
+        "spalte": "3"
+    },
+    {
+        "ID": "FR",
+        "NAME": "Friedrich\u00a0III.",
+        "start": "1415 Sep 21",
+        "end": "1493 Aug 19",
+        "spalte": "0"
+    },
+    {
+        "ID": "J1",
+        "NAME": "Joseph\u00a0I.",
+        "start": "1678 Jul 26",
+        "end": "1711 Apr 17",
+        "spalte": "0"
+    },
+    {
+        "ID": "JC",
+        "NAME": "Johann Karl",
+        "start": "1605 Nov 01",
+        "end": "1619 Dez 26",
+        "spalte": "2"
+    },
+    {
+        "ID": "L1",
+        "NAME": "Leopold\u00a0I.",
+        "start": "1640 Jun 09",
+        "end": "1705 Mai 05",
+        "spalte": "0"
+    },
+    {
+        "ID": "LW",
+        "NAME": "Leopold Wilhelm",
+        "start": "1614 Jan 05",
+        "end": "1662 Nov 20",
+        "spalte": "0"
+    },
+    {
+        "ID": "M1",
+        "NAME": "Maximilian\u00a0I.",
+        "start": "1459 Mar 22",
+        "end": "1519 Jan 12",
+        "spalte": "0"
+    },
+    {
+        "ID": "M2",
+        "NAME": "Maximilian\u00a0II.",
+        "start": "1527 Jul 31",
+        "end": "1576 Okt 12",
+        "spalte": "0"
+    },
+    {
+        "ID": "MAT",
+        "NAME": "Antonia",
+        "start": "1669 Jan 18",
+        "end": "1692 Dez 24",
+        "spalte": "1"
+    },
+    {
+        "ID": "MS",
+        "NAME": "Matthias",
+        "start": "1557 Feb 24",
+        "end": "1619 Mar 20",
+        "spalte": "0"
+    },
+    {
+        "ID": "MT",
+        "NAME": "Maria Theresia",
+        "start": "1717 Mai 13",
+        "end": "1780 Nov 29",
+        "spalte": "0"
+    },
+    {
+        "ID": "R2",
+        "NAME": "Rudolf\u00a0II.",
+        "start": "1552 Jul 18",
+        "end": "1612 Jan 20",
+        "spalte": "0"
+    }
+]
