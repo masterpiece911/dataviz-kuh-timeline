46e708eac6c7b0a219d8c1592d1161364485801c
Noyan Tillman Sahin <sahin.noyan@hotmail.com>
2020-01-27 23:41:21 +0100

    paar funktionierende Abfragen

--- frontend/src/data/query.js
+++ frontend/src/data/query.js
@@ -1,28 +1,76 @@
 import { personen } from './personen'
 
+const [start, end] = [1503, 1705];
+
 export function checkForOrt(category, place) {
     if (category === "gestorben") {
-        checkForSOrt(place)
+        return checkForSOrt(place)
     }
     if (category === "geboren") {
-        checkForGOrt(place)
+        return checkForGOrt(place)
     }
 }
 
 export function checkCategory(category, court) {
+    let persons;
     if (category === "Bevölkerung") {
-        checkForKaiser(court);
+        persons = checkForKaiser(court);
+        let yearObject = {};
+        let yearArray = [];
+        let maxVal = 0;
+        for (let index = start; index <= end; index += 1) {
+            yearObject[index] = persons.filter((person) => person.Geburtsdatum <= index && index <= person.Todesdatum);
+            if (maxVal < yearObject[index].length){
+                maxVal = yearObject[index].length;
+            }
+            yearArray.push({ x: index, y: yearObject[index].length });
+        }
+        return { persons: yearObject, graph: yearArray, max: maxVal };
+
     }
     if (category === "Durschnittsalter") {
-        checkForKaiser(court)
+        persons = checkForKaiser(court)
+        let yearObject = {};
+        let yearArray = [];
+        let maxVal = 0;
+        let sum = 0;
+        let val = 0;
+        for (let year = start; year <= end; year += 1) {
+            yearObject[year] = persons.filter((person) => person.Geburtsdatum <= year && year <= person.Todesdatum);
+            sum = yearObject[year].reduce((prev, person) => {return(prev +year - person.Geburtsdatum)}, 0);
+            val = sum / yearObject[year].length;
+            if (isNaN(val)) {
+                val = 0;
+            }
+            if (maxVal < val) {
+                maxVal = val;
+            }
+            yearArray.push({x: year, y: val});
+        }
+        return { persons: yearObject, graph: yearArray, max: maxVal};
     }
     if (category === "Anzahl der Geburten") {
-        bornIn(category)
+        persons = bornIn(category)
     }
     if (category === "Anzahl der Tode") {
-        bornIn(category)
+        persons = bornIn(category)
     }
 
+    let yearObject = {};
+    let yearArray = [];
+    for (let index = start; index <= end; index += 1) {
+        yearObject[index] = persons.filter((person) => person.Geburtsdatum <= index && index <= person.Todesdatum);
+        yearArray.push({ x: index, y: yearObject[index].length });
+    }
+
+    return { persons: yearObject, graph: yearArray };
+
+
+}
+
+function checkForAlive(year) {
+    const result = personen.filter(person => ((person.Geburtsdatum <= year) && (person.Todesdatum >= year)));
+    return result;
 }
 
 function checkForGOrt(place) {
@@ -45,14 +93,11 @@ function checkForSOrt(place) {
     return filtered;
 }
 
-function checkForAlive(year) {
-    const result = personen.filter(person => ((person.Geburtsdatum <= year) && (person.Todesdatum >= year)));
+function checkForKaiser(court) {
+    const result = personen.filter(person => {
 
-    return result;
-}
-
-function checkForKaiser(kaiserid) {
-    const result = personen.filter(person => (person.HofherrID === kaiserid));
+        return (person.HofherrID === court.ID)
+    });
 
     return result;
 }
@@ -64,53 +109,6 @@ export function checkForAmt(amt, kaiserid) {
 
     return result;
 }
-
-function checkForGeburtso(gort) {
-    const result = personen.filter(person => (person.Geburtsort === gort));
-
-    return result;
-}
-
-function checkForSterbeo(sort) {
-    const result = personen.filter(person => (person.Sterbeort === sort))
-
-    return result;
-}
-
-function checkForAliveUnder(start, end, kaiserid) {
-    let filtered = personen
-        .filter(person => ((person.Geburtsdatum <= start) && (person.Todesdatum >= end)))
-        .filter(person => (person.HofherrID === kaiserid))
-    return filtered;
-}
-
-function checkForBornUnder(year, kaiserid) {
-    let filtered = personen
-        .filter(person => ((person.Geburtsdatum <= year) && (person.Todesdatum >= year)))
-        .filter(person => (person.HofherrID === kaiserid))
-    return filtered;
-}
-
-function checkForWorkedUnder(amt, kaiserid) {
-    let filtered = personen
-        .filter(person => ((person.Amt <= amt)))
-        .filter(person => (person.HofherrID === kaiserid))
-    return filtered;
-}
-
-function checkForComeFromUnder(kaiserid, gort) {
-    let filtered = personen
-        .filter(person => (person.HofherrID === kaiserid))
-        .filter(person => (person.Geburtsort === gort))
-    return filtered;
-}
-
-function checkForDeathsUnder(year, kaiserid) {
-    let filtered = personen.filter(person => ((person.Geburtsdatum <= year) && (person.Todesdatum <= year)))
-        .filter(person => (person.HofherrID === kaiserid))
-    return filtered;
-}
-
 function bornIn(category) {
     var geburtsjahre = new Map();
     var todesjahre = new Map();
