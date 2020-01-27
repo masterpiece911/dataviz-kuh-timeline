12bfa986934821855ef94e3808ea0d7afcafb4ce
lex roodsar <a.vahid@campus.lmu.de>
2020-01-27 18:00:02 +0100

    query, data query

--- frontend/src/data/queryDefinition.js
+++ frontend/src/data/queryDefinition.js
@@ -1,68 +1,65 @@
-import {kaiser} from './kaiser';
+import { kaiser } from './kaiser';
+import { checkCaegory } from './data/query';
+import { checkForOrt, checkForAmt } from './query';
 
-export const queries = (setParamOne, setParamTwo) => [
-  {
-    params: [
-      {
-        name: `Kategorie`,
-        listOfItems: [
-          "Bevoelkerung",
-          "Alter",
-          "Geburten",
-          "Tode",
-          "Herkunftsort",
-          "Amt"
+export const queries = (setParamOne, setParamTwo) => [{
+        params: [{
+            name: `Kategorie`,
+            listOfItems: [
+                "Bevoelkerung",
+                "Alter",
+                "Geburten",
+                "Tode",
+                "Herkunftsort",
+                "Amt"
+            ],
+            initialValue: 'Bevoelkerung',
+            setter: (value) => { setParamOne(value) } // TODO CHANGE FUNCTION
+        }],
+        title: (category) => {}, // TODO TITLE FUNCTION
+        data: (category, court) => { checkCaegory(category, court) }, // TODO DATA FUNCTION
+        name: `Demographie` // TODO NAME
+    },
+    {
+        params: [{
+                name: `Kategorie`,
+                listOfItems: [
+                    "Sterbeort",
+                    "Herkunftsort"
+                ],
+                initialValue: 'Herkunftsort',
+                setter: (value) => { setParamOne(value) } //TODO CHANGE FUNCTION
+            },
+            {
+                name: `Ort`,
+                listOfItems: [
+                    // TODO LISTE AN ORTEN
+                ],
+                initialValue: '',
+                setter: (value) => { setParamTwo(value) } //TODO CHANGE FUNCTION
+            },
         ],
-        initialValue: 'Bevoelkerung',
-        setter: (value) => {setParamOne(value)} // TODO CHANGE FUNCTION
-      }
-    ],
-    title: (category) => {}, // TODO TITLE FUNCTION
-    data: () => {},// TODO DATA FUNCTION
-    name: `Demographie` // TODO NAME
-  },
-  {
-    params: [
-      {
-        name: `Kategorie`,
-        listOfItems: [
-          "Sterbeort",
-          "Herkunftsort"
+        title: (category, place) => {}, // TODO TITLE FUNCTION
+        data: (category, place) => { checkForOrt(category, place) }, // TODO DATA FUNCTION
+        name: `Geographie`
+    },
+    {
+        params: [{
+                name: `Amt`,
+                listOfItems: [
+                    // TODO LISTE AN AEMTERN
+                ],
+                initialValue: '',
+                setter: (value) => { setParamOne(value) } // TODO CHANGE FUNCTION
+            },
+            {
+                name: `Hofstaat`,
+                listOfItems: kaiser,
+                setter: (value) => { setParamTwo(value) }
+            }
         ],
-        initialValue: 'Herkunftsort',
-        setter: (value) => {setParamOne(value)}//TODO CHANGE FUNCTION
-      },
-      {
-        name: `Ort`,
-        listOfItems: [
-          // TODO LISTE AN ORTEN
-        ],
-        initialValue: '',
-        setter: (value) => {setParamTwo(value)}//TODO CHANGE FUNCTION
-      },
-    ],
-    title: (category, place) => {}, // TODO TITLE FUNCTION
-    data: () => {}, // TODO DATA FUNCTION
-    name: `Geographie`
-  },
-  {
-    params: [
-      {
-        name: `Amt`,
-        listOfItems: [
-          // TODO LISTE AN AEMTERN
-        ],
-        initialValue: '',
-        setter: (value) => {setParamOne(value)}// TODO CHANGE FUNCTION
-      },
-      {
-        name: `Hofstaat`,
-        listOfItems: kaiser,
-        setter: (value) => {setParamTwo(value)}
-      }
-    ],
-    title: (office, court) => {},
-    data: () => {},
-    name: `Amt`
-  }
+        title: (office, court) => {},
+        data: (office, court) => { checkForAmt(office, court) },
+        name: `Amt`
+    }
 ]
