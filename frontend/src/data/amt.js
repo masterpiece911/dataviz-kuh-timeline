1724b794337f195cb503ef1cdc1e77426c91b2f8
lex roodsar <a.vahid@campus.lmu.de>
2020-01-27 18:46:17 +0100

    aemter liste

---
+++ frontend/src/data/amt.js
@@ -0,0 +1,250 @@
+export const aemter = [{
+        "Hofamt": "Edeljungfrau"
+    },
+    {
+        "Hofamt": "Edelknabe"
+    },
+    {
+        "Hofamt": "Vorschneider"
+    },
+    {
+        "Hofamt": "Geheimer Rat"
+    },
+    {
+        "Hofamt": "Hofdiener"
+    },
+    {
+        "Hofamt": "Hofdiener (ohne Besoldung)"
+    },
+    {
+        "Hofamt": "Hofdiener (auf 1 Pferd)"
+    },
+    {
+        "Hofamt": "Hofdiener (auf 2 Pferden)"
+    },
+    {
+        "Hofamt": "Hofdiener (auf 3 und mehr Pferden)"
+    },
+    {
+        "Hofamt": "Hartschierhauptmann"
+    },
+    {
+        "Hofamt": "Hofbuchhalter"
+    },
+    {
+        "Hofamt": "Hofkanzler"
+    },
+    {
+        "Hofamt": "Hofsekret\u00e4r"
+    },
+    {
+        "Hofamt": "Hofsekret\u00e4r (burgundischer Expedition)"
+    },
+    {
+        "Hofamt": "Hofsekret\u00e4r (deutscher Expedition f\u00fcr B\u00f6hmen)"
+    },
+    {
+        "Hofamt": "Hofsekret\u00e4r (b\u00f6hmischer Expedition)"
+    },
+    {
+        "Hofamt": "Hofsekret\u00e4r (spanischer Expedition)"
+    },
+    {
+        "Hofamt": "Hofsekret\u00e4r (ungarischer Expedition)"
+    },
+    {
+        "Hofamt": "Hofsekret\u00e4r (lateinischer Expedition)"
+    },
+    {
+        "Hofamt": "Hofsekret\u00e4r (f\u00fcr Lausitz)"
+    },
+    {
+        "Hofamt": "Hofsekret\u00e4r (f\u00fcr Schlesien)"
+    },
+    {
+        "Hofamt": "Hofkammerdiener"
+    },
+    {
+        "Hofamt": "Hofkammerkonzipist"
+    },
+    {
+        "Hofamt": "Hofkammerpr\u00e4sident"
+    },
+    {
+        "Hofamt": "Hofkammerpr\u00e4sident (Amtsverwalter)"
+    },
+    {
+        "Hofamt": "Hofkammerrat"
+    },
+    {
+        "Hofamt": "Hofkammerregistrator"
+    },
+    {
+        "Hofamt": "Hofkammerregistrator (Adjunkt)"
+    },
+    {
+        "Hofamt": "Hofkammersekret\u00e4r"
+    },
+    {
+        "Hofamt": "Hofkammeruntersekret\u00e4r"
+    },
+    {
+        "Hofamt": "Hofkammervizepr\u00e4sident"
+    },
+    {
+        "Hofamt": "Oberstmustermeister"
+    },
+    {
+        "Hofamt": "Hofkriegsratspr\u00e4sident"
+    },
+    {
+        "Hofamt": "Hofquartiermeister"
+    },
+    {
+        "Hofamt": "Hofkriegsrat"
+    },
+    {
+        "Hofamt": "Hofkriegsratssekret\u00e4r"
+    },
+    {
+        "Hofamt": "Untermustermeister"
+    },
+    {
+        "Hofamt": "Hofkriegsratsvizepr\u00e4sident"
+    },
+    {
+        "Hofamt": "Hofkriegszahlmeister"
+    },
+    {
+        "Hofamt": "Hofkontrolleur"
+    },
+    {
+        "Hofamt": "Hofpfennigmeister"
+    },
+    {
+        "Hofamt": "Hofpostmeister"
+    },
+    {
+        "Hofamt": "Hofpostmeister (Amtsverwalter)"
+    },
+    {
+        "Hofamt": "Hofrat"
+    },
+    {
+        "Hofamt": "Huschier"
+    },
+    {
+        "Hofamt": "Hofvizekanzler"
+    },
+    {
+        "Hofamt": "Hofzahlamtskontrolleur"
+    },
+    {
+        "Hofamt": "Hofzahlmeister"
+    },
+    {
+        "Hofamt": "K\u00e4mmerer"
+    },
+    {
+        "Hofamt": "K\u00e4mmerer (unwirklich)"
+    },
+    {
+        "Hofamt": "Kammerdiener"
+    },
+    {
+        "Hofamt": "Leibwarterin"
+    },
+    {
+        "Hofamt": "Hofmathematiker"
+    },
+    {
+        "Hofamt": "Hofarzt"
+    },
+    {
+        "Hofamt": "Mundschenk"
+    },
+    {
+        "Hofamt": "Oberstfalkenmeister"
+    },
+    {
+        "Hofamt": "Obersthofmeister"
+    },
+    {
+        "Hofamt": "Obersthofmarschall"
+    },
+    {
+        "Hofamt": "Obersthofmeisterin"
+    },
+    {
+        "Hofamt": "Obersthofmeister (Amtsverwalter)"
+    },
+    {
+        "Hofamt": "Oberstj\u00e4germeister"
+    },
+    {
+        "Hofamt": "Oberstk\u00e4mmerer"
+    },
+    {
+        "Hofamt": "Oberstk\u00fcchenmeister"
+    },
+    {
+        "Hofamt": "Oberstsilberk\u00e4mmerer"
+    },
+    {
+        "Hofamt": "Oberstsilberk\u00e4mmerer (Amtsverwalter)"
+    },
+    {
+        "Hofamt": "Oberststallmeister"
+    },
+    {
+        "Hofamt": "St\u00e4belmeister"
+    },
+    {
+        "Hofamt": "Unterst\u00e4belmeister"
+    },
+    {
+        "Hofamt": "Panetier"
+    },
+    {
+        "Hofamt": "Rat"
+    },
+    {
+        "Hofamt": "Reichskanzleisekret\u00e4r"
+    },
+    {
+        "Hofamt": "Reichshoffiskal"
+    },
+    {
+        "Hofamt": "Reichshofratspr\u00e4sident"
+    },
+    {
+        "Hofamt": "Reichshofrat"
+    },
+    {
+        "Hofamt": "Reichshofratssekret\u00e4r"
+    },
+    {
+        "Hofamt": "Reichshofratsvizepr\u00e4sident"
+    },
+    {
+        "Hofamt": "Schatzmeister"
+    },
+    {
+        "Hofamt": "Sommelier"
+    },
+    {
+        "Hofamt": "Trabantenhauptmann"
+    },
+    {
+        "Hofamt": "Truchsess"
+    },
+    {
+        "Hofamt": "Untermarschall"
+    },
+    {
+        "Hofamt": "Untersilberk\u00e4mmerer"
+    },
+    {
+        "Hofamt": "Vogelj\u00e4ger"
+    }
+]
