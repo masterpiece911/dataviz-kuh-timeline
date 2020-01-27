fa8f4c92ed30db436b4fc153116cc10d854e019e
lex roodsar <a.vahid@campus.lmu.de>
2020-01-27 14:30:33 +0100

    query verschacthelte filter

--- frontend/src/data/query.js
+++ frontend/src/data/query.js
@@ -7,25 +7,53 @@ function checkForAlive(year) {
 }
 
 function checkForKaiser(kaiserid) {
-    const result = personen.filter(person => (personen.HofherrID == kaiserid));
+    const result = personen.filter(person => (personen.HofherrID === kaiserid));
 
     return result;
 }
 
 function checkForAmt(amt) {
-    const result = personen.filter(person => (personen.Amt == amt));
+    const result = personen.filter(person => (personen.Amt === amt));
 
     return result;
 }
 
 function checkForGeburtso(gort) {
-    const result = personen.filter(person => (personen.Geburtsort == gort));
+    const result = personen.filter(person => (personen.Geburtsort === gort));
 
     return result;
 }
 
 function checkForSterbeo(sort) {
-    const result = personen.filter(person => (personen.Sterbeort == sort));
+    const result = personen.filter(person => (personen.Sterbeort === sort))
 
     return result;
+}
+
+function checkForAliveUnder(start, end, kaiserid) {
+    let filtered = personen
+        .filter(person => ((personen.Geburtsdatum <= start) && (personen.Todesdatum >= end)))
+        .filter(person => (personen.HofherrID === kaiserid))
+    return filtered;
+}
+
+function checkForBornUnder(year, kaiserid) {
+    let filtered = personen
+        .filter(person => ((personen.Geburtsdatum <= year) && (personen.Todesdatum >= year)))
+        .filter(person => (personen.HofherrID === kaiserid))
+    return filtered;
+}
+
+function checkForWorkedUnder(amt, kaiserid) {
+    let filtered = personen
+        .filter(person => ((personen.Amt <= amt)))
+        .filter(person => (personen.HofherrID === kaiserid))
+    return filtered;
+}
+
+function checkForComeFromUnder(kaiserid, gort) {
+    let filtered = personen
+        .filter(person => (personen.HofherrID === kaiserid))
+        .filter(person => (personen.Geburtsort === gort))
+    return filtered;
 }
