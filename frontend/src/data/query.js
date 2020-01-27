cbe69d2572e98b7db3f3b2270e5c43205c573211
lex roodsar <a.vahid@campus.lmu.de>
2020-01-27 14:43:26 +0100

    query fix

--- frontend/src/data/query.js
+++ frontend/src/data/query.js
@@ -1,59 +1,59 @@
-import personen from personen.js
+import personen from './data/personen'
 
 function checkForAlive(year) {
-    const result = personen.filter(person => ((personen.Geburtsdatum <= year) && (personen.Todesdatum >= year)));
+    const result = personen.filter(person => ((person.Geburtsdatum <= year) && (person.Todesdatum >= year)));
 
     return result;
 }
 
 function checkForKaiser(kaiserid) {
-    const result = personen.filter(person => (personen.HofherrID === kaiserid));
+    const result = personen.filter(person => (person.HofherrID === kaiserid));
 
     return result;
 }
 
 function checkForAmt(amt) {
-    const result = personen.filter(person => (personen.Amt === amt));
+    const result = personen.filter(person => (person.Amt === amt));
 
     return result;
 }
 
 function checkForGeburtso(gort) {
-    const result = personen.filter(person => (personen.Geburtsort === gort));
+    const result = personen.filter(person => (person.Geburtsort === gort));
 
     return result;
 }
 
 function checkForSterbeo(sort) {
-    const result = personen.filter(person => (personen.Sterbeort === sort))
+    const result = personen.filter(person => (person.Sterbeort === sort))
 
     return result;
 }
 
 function checkForAliveUnder(start, end, kaiserid) {
     let filtered = personen
-        .filter(person => ((personen.Geburtsdatum <= start) && (personen.Todesdatum >= end)))
-        .filter(person => (personen.HofherrID === kaiserid))
+        .filter(person => ((person.Geburtsdatum <= start) && (person.Todesdatum >= end)))
+        .filter(person => (person.HofherrID === kaiserid))
     return filtered;
 }
 
 function checkForBornUnder(year, kaiserid) {
     let filtered = personen
-        .filter(person => ((personen.Geburtsdatum <= year) && (personen.Todesdatum >= year)))
-        .filter(person => (personen.HofherrID === kaiserid))
+        .filter(person => ((person.Geburtsdatum <= year) && (person.Todesdatum >= year)))
+        .filter(person => (person.HofherrID === kaiserid))
     return filtered;
 }
 
 function checkForWorkedUnder(amt, kaiserid) {
     let filtered = personen
-        .filter(person => ((personen.Amt <= amt)))
-        .filter(person => (personen.HofherrID === kaiserid))
+        .filter(person => ((person.Amt <= amt)))
+        .filter(person => (person.HofherrID === kaiserid))
     return filtered;
 }
 
 function checkForComeFromUnder(kaiserid, gort) {
     let filtered = personen
-        .filter(person => (personen.HofherrID === kaiserid))
-        .filter(person => (personen.Geburtsort === gort))
+        .filter(person => (person.HofherrID === kaiserid))
+        .filter(person => (person.Geburtsort === gort))
     return filtered;
 }
