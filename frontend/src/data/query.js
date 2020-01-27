a0ef40e8b3c53ca5c2442368bf5076d760cb7262
lex roodsar <a.vahid@campus.lmu.de>
2020-01-27 14:06:12 +0100

    query with implemented filter

--- frontend/src/data/query.js
+++ frontend/src/data/query.js
@@ -1,21 +1,31 @@
 import personen from personen.js
 
 function checkForAlive(year) {
-    return (personen.Geburtsdatum <= year && personen.Todesdatum >= year);
+    const result = personen.filter(person => ((personen.Geburtsdatum <= year) && (personen.Todesdatum >= year)));
+
+    return result;
 }
 
 function checkForKaiser(kaiserid) {
-    return (personen.HofherrID == kaiserid)
+    const result = personen.filter(person => (personen.HofherrID == kaiserid));
+
+    return result;
 }
 
 function checkForAmt(amt) {
-    return (personen.Amt == amt)
+    const result = personen.filter(person => (personen.Amt == amt));
+
+    return result;
 }
 
 function checkForGeburtso(gort) {
-    return (personen.Geburtsort == gort);
+    const result = personen.filter(person => (personen.Geburtsort == gort));
+
+    return result;
 }
 
 function checkForSterbeo(sort) {
-    return (personen.Sterbeort == sort);
+    const result = personen.filter(person => (personen.Sterbeort == sort));
+
+    return result;
 }
