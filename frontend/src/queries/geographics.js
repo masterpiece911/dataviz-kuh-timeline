4a0cb89fb327c98e8d4966cb777f9e0cd0f1f91a
Alex V <a.vahid@campus.lmu.de>
2020-01-30 03:04:26 +0100

    geographie query ansatz

--- frontend/src/queries/geographics.js
+++ frontend/src/queries/geographics.js
@@ -1,43 +1,105 @@
 import { Orte } from '../data/orte';
 import { personen } from '../data/personen';
 import { start, end } from './definitions';
+import { filterUnknownBirthPlaces, filterUnknownDeathPlaces } from './utilities';
+
 
 const diedIn = 'Todesort';
 const bornIn = 'Geburtsort';
 
 const query = (setParamOne, setParamTwo) => {
-  return (
-    {
-      params: [{
-        name: `Kategorie`,
-        listOfItems: [
-          diedIn,
-          bornIn,
+    return ({
+        params: [{
+                name: `Kategorie`,
+                listOfItems: [
+                    diedIn,
+                    bornIn,
+                ],
+                initialValue: diedIn,
+                setter: (value) => { setParamOne(value) }
+            },
+            {
+                name: `Ort`,
+                listOfItems: Orte,
+                field: 'Ort',
+                initialValue: Orte[1],
+                setter: (value) => { setParamTwo(value) }
+            },
         ],
-        initialValue: diedIn,
-        setter: (value) => { setParamOne(value) }
-      },
-      {
-        name: `Ort`,
-        listOfItems: Orte,
-        field: 'Ort',
-        initialValue: Orte[1],
-        setter: (value) => { setParamTwo(value) }
-      },
-      ],
-      title: (category, place) => `Höflinge ${category} in ${place}`,
-      data: (category, place) => { return geographicalData(category, place) },
-      name: `Geographie`
-    }
-  )
+        title: (category, place) => `Höflinge ${category} in ${place}`,
+        data: (category, place) => { return geographicalData(category, place) },
+        name: `Geographie`
+    })
 }
 
 const geographicalData = (category, place) => {
 
-  let persons;
-  // TODO
-  throw "NOT IMPLEMENTED YET";
-  // return {graph: [], persons: {}, max: 0}
+    let persons;
+    // TODO
+    if (category === diedIn) {
+        persons = personen
+            .filter(filterUnknownDeathPlaces)
+            .map((person) => {
+                person['placeOfDeath'] = person.Sterbeort;
+                return person;
+            });
+
+
+        let yearObject = {};
+        let yearArray = [];
+        let maxVal = 0;
+        let val = 0;
+        let min = parseInt(court.start.substring(0, 4));
+        let max = parseInt(court.end.substring(0, 4));
+        for (let year = min; year <= max; year += 1) {
+            yearObject[year] = persons.filter((person) => person.placeOfDeath === place);
+            val++;
+            if (isNaN(val)) {
+                val = 0;
+            }
+            if (maxVal < val) {
+                maxVal = val;
+            }
+            yearArray.push({ x: year, y: val });
+        }
+        return { persons: yearObject, graph: yearArray, max: maxVal };
+    }
+    if (category === bornIn) {
+        persons = personen
+            .filter(filterUnknownBirthPlaces)
+            .map((person) => {
+                person['placeOfBirth'] = person.Geburtsort;
+                return person;
+            });
+
+
+        let yearObject = {};
+        let yearArray = [];
+        let maxVal = 0;
+        let val = 0;
+        let min = parseInt(court.start.substring(0, 4));
+        let max = parseInt(court.end.substring(0, 4));
+        for (let year = min; year <= max; year += 1) {
+            yearObject[year] = persons.filter((person) => person.placeOfBirth === place);
+            val++;
+            if (isNaN(val)) {
+                val = 0;
+            }
+            if (maxVal < val) {
+                maxVal = val;
+            }
+            yearArray.push({ x: year, y: val });
+        }
+        return { persons: yearObject, graph: yearArray, max: maxVal };
+    }
+
+
+
+
+
+
+    //throw "NOT IMPLEMENTED YET";
+    // return {graph: [], persons: {}, max: 0}
 
 }
 
--- frontend/src/queries/utilities.js
+++ frontend/src/queries/utilities.js
@@ -1,3 +1,5 @@
 export const filterForCourt = (court) => (item) => item.HofherrID === court.ID;
 export const filterUnknownDeaths = (item) => item.Todesdatum !== "";
 export const filterUnknownBirths = (item) => item.Geburtsdatum !== "";
+export const filterUnknownDeathPlaces = (item) => item.Sterbeort !== "";
+export const filterUnknownBirthPlaces = (item) => item.Geburtsort !== "";
