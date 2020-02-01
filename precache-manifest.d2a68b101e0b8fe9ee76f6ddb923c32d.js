95f8c72051aa4f5ed617d3d8ec30671a4f6f9fdf
Noyan Tillman Sahin <sahin.noyan@hotmail.com>
2020-02-01 21:36:25 +0100

    Updates

---
+++ precache-manifest.d2a68b101e0b8fe9ee76f6ddb923c32d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "525679cc46ed7694c439fe62541819cd",
+    "url": "/dataviz-kuh-timeline/index.html"
+  },
+  {
+    "revision": "74dca98096e829f07cce",
+    "url": "/dataviz-kuh-timeline/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "a0739b2ff2673d684c0d",
+    "url": "/dataviz-kuh-timeline/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "74dca98096e829f07cce",
+    "url": "/dataviz-kuh-timeline/static/js/2.30f11e8a.chunk.js"
+  },
+  {
+    "revision": "fd56044fbab90a74509299bbf81211c5",
+    "url": "/dataviz-kuh-timeline/static/js/2.30f11e8a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a0739b2ff2673d684c0d",
+    "url": "/dataviz-kuh-timeline/static/js/main.ac5376f1.chunk.js"
+  },
+  {
+    "revision": "975d7bbda1ea4c7761a0",
+    "url": "/dataviz-kuh-timeline/static/js/runtime-main.6a9c0da5.js"
+  }
+]);
